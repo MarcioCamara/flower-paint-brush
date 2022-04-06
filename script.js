1b8a67159fc78125e0adc670c436a04ce10d11eb
MarcioCamara <marciocamarajr@gmail.com>
2022-04-06 07:56:49 -0300

    feat: adicionado aviso e botão de ok

--- script.js
+++ script.js
@@ -7,13 +7,59 @@ canvas.height = window.innerHeight;
 
 const simultaneousRoots = 3;
 let drawing = false;
-
-ctx.lineWidth = 0.4;
+let canDraw = false;
 
 ctx.font = 0.05 * window.innerWidth + "px Arial";
-console.log(window.innerWidth);
 ctx.fillText('click and drag (or touch) anywhere', 30, 0.15 * window.innerWidth);
 ctx.fillText('on the screen to start drawing (:', 30, 0.20 * window.innerWidth);
+
+const rect = {
+  x: 30,
+  y: 0.25 * window.innerWidth - 0.034 * window.innerWidth,
+  w: 0.1 * window.innerWidth,
+  h: 0.05 * window.innerWidth,
+};
+
+// begin ok button
+ctx.fillText('OK', rect.x + 0.013 * window.innerWidth, rect.y + 0.044 * window.innerWidth);
+ctx.lineWidth = 0.0045 * window.innerWidth;
+ctx.rect(rect.x, rect.y, rect.w, rect.h);
+ctx.stroke();
+
+canvas.addEventListener('click', checkStart, false);
+
+go = false;
+function checkStart(e) {
+  const p = getMousePos(e);
+
+  if (isInsideRange(p)) {
+    go = !go;
+    if (go) {
+      hideText();
+    }
+  }
+}
+
+function getMousePos(e) {
+  const r = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - r.left,
+    y: e.clientY - r.top
+  };
+}
+
+function isInsideRange(p) {
+  return (p.x >= rect.x && p.x <= rect.x + rect.w &&
+    p.y >= rect.y && p.y <= rect.y + rect.h);
+}
+
+function hideText() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canDraw = true;
+}
+// end ok button
+
+ctx.lineWidth = 0.4;
 
 window.addEventListener('mousemove', function (event) {
   if (drawing) {
@@ -25,6 +71,10 @@ window.addEventListener('mousemove', function (event) {
 });
 
 window.addEventListener('mousedown', function (event) {
+  if (!canDraw) {
+    return;
+  }
+
   drawing = true;
 
   for (let i = 0; i < simultaneousRoots * 3; i++) {
